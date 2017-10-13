38207a0913ce925371acac84fb2c81c40784ff95
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-13 15:07:43 +0000

    Disable WMT17 test

--- tests/decoder/_wmt17/test_ende.sh
+++ tests/decoder/_wmt17/test_ende.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Test code goes here
-$MRT_MARIAN/build/s2s -c $MRT_MODELS/wmt17_systems/marian.en-de.yml < text.in | tail -n 99 > text.out
+$MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt17_systems/marian.en-de.yml < text.in | tail -n 99 > text.out
 diff text.out text.expected > text.diff
 
 # Exit with success code
