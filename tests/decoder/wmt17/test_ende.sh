7b240a925c4c820578603031171f978294010fb9
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-13 14:24:25 +0000

    Add MWT17 test

---
+++ tests/decoder/wmt17/test_ende.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+$MRT_MARIAN/build/s2s -c $MRT_MODELS/wmt17_systems/marian.en-de.yml < text.in | tail -n 99 > text.out
+diff text.out text.expected > text.diff
+
+# Exit with success code
+exit 0
