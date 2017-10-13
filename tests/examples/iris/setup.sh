bede612d47da10af304aa21bc57e36820d8ab3c6
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-13 20:22:30 +0000

    Run examples if available

---
+++ tests/examples/iris/setup.sh
@@ -0,0 +1,1 @@
+test -e $MRT_MARIAN/build/iris_example || exit 1
--- tests/examples/mnist/setup.sh
+++ tests/examples/mnist/setup.sh
@@ -4,6 +4,8 @@
 set -e
 
 # Setup code goes here
+test -e $MRT_MARIAN/build/mnist_example
+
 (( $(ls -1 *-ubyte 2>/dev/null | wc -l) == 4 )) && exit 0
 
 wget -nc http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
