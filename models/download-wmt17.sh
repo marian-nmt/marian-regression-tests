85620b17475fa30dfc8cfe4f3a950d0cc6a64262
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-16 09:16:35 +0000

    Update script

--- models/download-wmt17.sh
+++ models/download-wmt17.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Download single en-de wmt17 model
-wget -q --progress=dot --no-clobber -r -e robots=off -nH -np \
+wget -nv -nc -r -e robots=off -nH -np \
     -R *ens2* -R *ens3* -R *ens4* -R *r2l* -R translate-ensemble.sh -R translate-reranked.sh -R index.html* \
     http://data.statmt.org/wmt17_systems/en-de/
 # Download additional scripts
-wget -q --progress=dot --no-clobber -r -e robots=off -nH -np \
+wget -nv -nc -r -e robots=off -nH -np \
     -R index.html* \
     http://data.statmt.org/wmt17_systems/scripts/ http://data.statmt.org/wmt17_systems/vars
