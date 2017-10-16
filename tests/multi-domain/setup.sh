ece78ef8da10b7fd4401fbe7fc6e11a7d6634d04
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-16 10:57:28 +0000

    Add test for marian-multi-domain

---
+++ tests/multi-domain/setup.sh
@@ -0,0 +1,2 @@
+test -e $MRT_MARIAN/build/marian-multi-domain || exit 1
+test -f $MRT_MODELS/wmt16_systems/en-de/model.npz || exit 1
---
+++ tests/multi-domain/test_bleu_and_costs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -f domain.log
+
+#$MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml < ubuntu.in > trans.out
+#diff trans.out trans.expected > trans.diff
+#$MRT_TOOLS/moses-scripts/scripts/generic/multi-bleu.perl -lc ubuntu.ref < trans.out > trans.bleu
+#diff trans.bleu trans.bleu.expected > trans.bleu.diff
+
+
+# Run multi-domain
+$MRT_MARIAN/build/marian-multi-domain \
+  -m $MRT_MODELS/wmt16_systems/en-de/model.npz \
+  -v $MRT_MODELS/wmt16_systems/en-de/vocab.en.json -v $MRT_MODELS/wmt16_systems/en-de/vocab.de.json \
+  --dim-vocabs 85000 85000 --dim-emb 500 \
+  -t ubuntu.in ubuntu.ref --log domain.log < ubuntu.in > domain.out
+
+# Check outputs
+diff domain.out domain.expected > domain.diff
+
+# Check BLEU
+$MRT_TOOLS/moses-scripts/scripts/generic/multi-bleu.perl -lc ubuntu.ref < domain.out > domain.bleu
+diff domain.bleu domain.bleu.expected > domain.bleu.diff
+
+# Check costs
+cat domain.log | grep 'Ep\. ' | $MRT_TOOLS/strip-timestamps.sh | sed 's/ : Time.*//' > costs.out
+$MRT_TOOLS/diff-floats.py -p 0.01 costs.out costs.expected > costs.diff
+
+# Exit with success code
+exit 0
