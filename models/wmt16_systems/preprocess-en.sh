8e754043d1e0785d2601f200d3157765740a15c9
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-10-16 10:46:33 +0000

    Update preprocessing script

--- models/wmt16_systems/preprocess-en.sh
+++ models/wmt16_systems/preprocess-en.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-moses_scripts=$root/../tools/moses-scripts
-subword_nmt=$root/../tools/subword-nmt
+moses_scripts=$root/../../tools/moses-scripts
+subword_nmt=$root/../../tools/subword-nmt
 
-model_dir=$root/wmt16.en-de
+model_dir=$root/en-de
 
 $moses_scripts/scripts/tokenizer/normalize-punctuation.perl -l en \
-    | $moses_scripts/scripts/tokenizer/tokenizer.perl -l en -penn \
+    | $moses_scripts/scripts/tokenizer/tokenizer.perl -l en -penn -threads 16 \
     | $moses_scripts/scripts/recaser/truecase.perl -model $model_dir/truecase-model.en \
     | $subword_nmt/apply_bpe.py -c $model_dir/ende.bpe
