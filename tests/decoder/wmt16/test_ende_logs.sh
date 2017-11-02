26d5479499d90e8c30b277541cddd5b6a3ab5d77
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-11-02 09:39:28 +0000

    Add test for decoder logging

---
+++ tests/decoder/wmt16/test_ende_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+$MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml < text.in 2> logs.raw
+cat logs.raw | grep "] Best translation" | sed -r "s/.*Best translation [0-9]+ : (.*)/\1/" > logs.out
+diff logs.out text.expected > logs.diff
+
+# Exit with success code
+exit 0
