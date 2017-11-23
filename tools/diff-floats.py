823df6d38be15ebededef915b62d4139c7039542
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-11-23 21:40:41 +0000

    Add --max-diff-nums option

--- tools/diff-floats.py
+++ tools/diff-floats.py
@@ -16,6 +16,7 @@ def is_numeric(s):
 def main():
     args = parse_user_args()
     exit_code = 0
+    max_diff_nums = args.max_diff_nums
 
     for i, line1 in enumerate(args.file1):
         line2 = args.file2.next()
@@ -34,15 +35,20 @@ def main():
             continue
 
         if len(nums1) != len(nums2):
-            print "Line {}: different number of numerics: {} / {}"\
+            print "Line {}: different number of numerics: {} / {}" \
                 .format(i, nums1, nums2)
             exit_code = 1
             continue
 
         for j, (n1, n2) in enumerate(zip(nums1, nums2)):
             if abs(n1 - n2) > args.precision:
-                print "Line {}: {} != {}".format(i, n1, n2)
-                exit_code = 1
+                if max_diff_nums < 1:
+                    print "Line {}: {} != {}".format(i, n1, n2)
+                    exit_code = 1
+                else:
+                    print "Line {}: {} != {}, allowed diff. numbers: {}" \
+                        .format(i, n1, n2, max_diff_nums)
+                    max_diff_nums -= 1
 
     for _ in args.file2:
         print "Extra line in the second file!"
@@ -56,6 +62,7 @@ def parse_user_args():
     parser.add_argument("file1", type=argparse.FileType('r'))
     parser.add_argument("file2", type=argparse.FileType('r'))
     parser.add_argument("-p", "--precision", type=float, default=0.001)
+    parser.add_argument("-n", "--max-diff-nums", type=int, default=0)
     return parser.parse_args()
 
 
