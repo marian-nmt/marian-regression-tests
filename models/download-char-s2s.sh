7388e52e715a7e4c5d7f4fb142b966d1c5c723e6
Tomasz Dwojak <t.dwojak@amu.edu.pl>
2017-11-29 10:10:36 +0000

    change the name of the model

--- models/download-char-s2s.sh
+++ models/download-char-s2s.sh
@@ -4,7 +4,7 @@
 mkdir -p char-s2s
 cd char-s2s
 
-wget -nc -nv http://data.statmt.org/romang/marian-regression-tests/models/char-s2s/model.iter350000.npz
+wget -nc -nv http://data.statmt.org/romang/marian-regression-tests/models/char-s2s/model.npz
 wget -nc -nv http://data.statmt.org/romang/marian-regression-tests/models/char-s2s/translate.yml
 wget -nc -nv http://data.statmt.org/romang/marian-regression-tests/models/char-s2s/vocab.en.yml
 wget -nc -nv http://data.statmt.org/romang/marian-regression-tests/models/char-s2s/vocab.ro.yml
--- tests/models/char-s2s/setup.sh
+++ tests/models/char-s2s/setup.sh
@@ -1,2 +1,2 @@
-test -f $MRT_MODELS/char-s2s/model.iter350000.npz || exit 1
+test -f $MRT_MODELS/char-s2s/model.npz || exit 1
 test -f $MRT_MODELS/char-s2s/translate.yml || exit 1
