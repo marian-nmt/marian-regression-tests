3c00a62a8b6021cc10c08b9c175e4c00d61c1bf9
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-12-06 11:26:01 +0000

    Add tests for WMT16/17 decoding with larger batches

---
+++ tests/models/wmt16-ende/test_translation_b6n_batch32.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Run Marian
+$MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml -b 6 -n 1.0 \
+    --mini-batch 32 --maxi-batch 10 --maxi-batch-sort src -w 2500 \
+    < text.b6n.in > marian.batch32.out
+
+# Compare with Marian and Nematus
+diff marian.batch32.out marian.b6n.expected > marian.batch32.diff
+diff marian.batch32.out nematus.b6n.out > nematus.batch32.diff
+
+# Exit with success code
+exit 0
--- tests/models/wmt17-ende/test_translation_b6n_batch32.sh
+++ tests/models/wmt17-ende/test_translation_b6n_batch32.sh
@@ -3,19 +3,17 @@
 # Exit on error
 set -e
 
+#cp marian.batch32.out marian.batch32.out.$$ || true
 rm -f marian.batch32.out
 
 # Run Marian
 $MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt17_systems/marian.en-de.yml -b 6 -n 1.0 \
-    --mini-batch 32 --maxi-batch 100 --maxi-batch-sort src -w 2500 \
+    --mini-batch 32 --maxi-batch 10 --maxi-batch-sort src -w 2500 \
     < text.b6n.in > marian.batch32.out
 
-# Compare with Marian
+# Compare with Marian and Nematus
 diff marian.batch32.out marian.b6n.expected > marian.batch32.diff
-
-# Compare with Nematus
-# The very first line is ommitted as it may differ due to a bug with GPU memory allocation
-diff <(tail -n +2 marian.batch32.out) <(tail -n +2 nematus.b6n.out) > nematus.batch32.diff
+diff marian.batch32.out nematus.b6n.out > nematus.batch32.diff
 
 # Exit with success code
 exit 0
