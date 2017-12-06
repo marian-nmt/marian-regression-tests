125af1d6c503cef4403204c5febae00763eb37a5
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2017-12-06 11:26:01 +0000

    Add tests for WMT16/17 decoding with larger batches

---
+++ tests/models/wmt16-ende/test_translation_b6n_batch64.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Run Marian
+$MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml -b 6 -n 1.0 \
+    --mini-batch 64 --maxi-batch 10 --maxi-batch-sort src -w 2500 \
+    < text.b6n.in > marian.batch64.out
+
+# Compare with Marian and Nematus
+diff marian.batch64.out marian.b6n.expected > marian.batch64.diff
+diff marian.batch64.out nematus.b6n.out > nematus.batch64.diff
+
+# Exit with success code
+exit 0
--- tests/models/wmt17-ende/test_translation_b6n.sh
+++ tests/models/wmt17-ende/test_translation_b6n.sh
@@ -8,12 +8,9 @@ rm -f marian.b6n.out
 # Run Marian
 $MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt17_systems/marian.en-de.yml -b 6 -n 1.0 < text.b6n.in > marian.b6n.out
 
-# Compare with Marian
+# Compare with Marian and Nematus
 diff marian.b6n.out marian.b6n.expected > marian.b6n.diff
-
-# Compare with Nematus
-# The very first line is ommitted as it may differ due to a bug with GPU memory allocation
-diff <(tail -n +2 marian.b6n.out) <(tail -n +2 nematus.b6n.out) > nematus.b6n.diff
+diff marian.b6n.out nematus.b6n.out > nematus.b6n.diff
 
 # Exit with success code
 exit 0
