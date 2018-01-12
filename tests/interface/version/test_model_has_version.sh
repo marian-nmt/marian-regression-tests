148aac8fac6a8a513f85d409cc1d842297be5bd0
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-01-12 11:58:11 +0000

    Add tests checking model version

---
+++ tests/interface/version/test_model_has_version.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -rf version version.log
+mkdir -p version
+
+# Save a model
+$MRT_MARIAN/build/marian \
+    -m version/model.npz \
+    -t $MRT_DATA/europarl.de-en/corpus.bpe.en $MRT_DATA/europarl.de-en/corpus.bpe.de \
+    -v version/vocab.en.yml version/vocab.de.yml \
+    --after-batches 1
+
+# Check if the model contains a version
+test -e version/model.npz
+python3 $MRT_MARIAN/scripts/contrib/model_info.py -m version/model.npz | grep -qP "version: v[1-9]+\.[0-9]+\.[0-9]+\+.*"
+
+# Check if the version is printed
+echo "test" | $MRT_MARIAN/build/marian-decoder \
+    -m version/model.npz -v version/vocab.en.yml version/vocab.de.yml \
+    --log version.log
+
+test -e version.log
+grep -qP "created with Marian v[1-9]+\.[0-9]+\.[0-9]+\+.*" version.log
+
+# Exit with success code
+exit 0
