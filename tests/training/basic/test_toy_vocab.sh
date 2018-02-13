e931fcc28d443a0a1d2f75b4fad8762d6c004ceb
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-02-13 11:29:31 +0000

    Update tests after changes in batch sorting

--- tests/training/basic/test_toy_vocab.sh
+++ tests/training/basic/test_toy_vocab.sh
@@ -8,11 +8,9 @@ mkdir -p toy
 rm -f toy/* toy.log
 
 $MRT_MARIAN/build/marian \
-    -m toy/model.npz \
-    -t $MRT_DATA/europarl.de-en/toy.bpe.en $MRT_DATA/europarl.de-en/toy.bpe.de \
-    -v toy/vocab.en.yml toy/vocab.de.yml \
-    --log toy.log --disp-freq 5 -e 5 \
-    --seed 1111
+    --seed 1111 \
+    -m toy/model.npz -t $MRT_DATA/europarl.de-en/toy.bpe.{de,en} -v toy/vocab.{en,de}.yml \
+    --log toy.log --disp-freq 5 -e 5
 
 test -e toy/vocab.en.yml
 test -e toy/vocab.de.yml
@@ -21,7 +19,7 @@ test -e toy/model.npz.yml
 test -e toy/model.npz.amun.yml
 
 cat toy.log | $MRT_TOOLS/extract-costs.sh > toy.out
-$MRT_TOOLS/diff-floats.py toy.out toy.expected -p 0.9 > toy.diff
+$MRT_TOOLS/diff-floats.py toy.out toy.expected -p 0.99 -n 5 > toy.diff
 
 # Exit with success code
 exit 0
