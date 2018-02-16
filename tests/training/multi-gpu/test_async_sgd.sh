9cefe7eaa4d8dfd5766bd8a1c0ad6043c7eaeefa
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-02-16 20:20:09 +0000

    Update the multi-gpu test

--- tests/training/multi-gpu/test_async_sgd.sh
+++ tests/training/multi-gpu/test_async_sgd.sh
@@ -13,11 +13,9 @@ rm -rf async_sgd async_sgd.log
 mkdir -p async_sgd
 
 $MRT_MARIAN/build/marian \
-    --no-shuffle --seed 1111 \
+    --no-shuffle --seed 1111 --maxi-batch 1 --maxi-batch-sort none --dim-emb 128 --dim-rnn 256 \
     --devices 0 1 \
-    -m async_sgd/model.npz \
-    -t $MRT_DATA/europarl.de-en/corpus.bpe.en $MRT_DATA/europarl.de-en/corpus.bpe.de \
-    -v vocab.en.yml vocab.de.yml \
+    -m async_sgd/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.{en,de}.yml \
     --disp-freq 20 --after-batches 400 \
     --log async_sgd.log
 
@@ -26,7 +24,7 @@ test -e vocab.de.yml
 test -e async_sgd.log
 
 cat async_sgd.log | $MRT_TOOLS/strip-timestamps.sh | grep -oP "Ep\. 1 .* Cost [0-9.]*" > async_sgd.out
-$MRT_TOOLS/diff-floats.py async_sgd.out async_sgd.expected -p 6.00 --max-diff-nums 2 > async_sgd.diff
+$MRT_TOOLS/diff-floats.py async_sgd.out async_sgd.expected -p 5.00 --max-diff-nums 2 > async_sgd.diff
 
 # Exit with success code
 exit 0
