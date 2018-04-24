4e0d3451d9f420bed7f1b9d3e137d4ec4705f557
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-04-24 08:59:51 +0000

    Fix multi-node tests

--- tests/training/restart/test_saving_adam_params_sync.sh
+++ tests/training/restart/test_saving_adam_params_sync.sh
@@ -31,8 +31,8 @@ diff adam_sync.keys.out adam.keys.expected > adam_sync.keys.diff
 python $MRT_MARIAN/scripts/contrib/model_info.py -m adam_sync/model.npz.optimizer.npz -k "adam_mt" > adam_sync.mt.out
 python $MRT_MARIAN/scripts/contrib/model_info.py -m adam_sync/model.npz.optimizer.npz -k "adam_vt" > adam_sync.vt.out
 
-$MRT_TOOLS/diff-floats.py -p 0.001  adam_sync.mt.out adam_sync.mt.expected > adam_sync.mt.diff
-$MRT_TOOLS/diff-floats.py -p 0.0001 adam_sync.vt.out adam_sync.vt.expected > adam_sync.vt.diff
+$MRT_TOOLS/diff-floats.py --numpy -p 0.001  adam_sync.mt.out adam_sync.mt.expected > adam_sync.mt.diff
+$MRT_TOOLS/diff-floats.py --numpy -p 0.0001 adam_sync.vt.out adam_sync.vt.expected > adam_sync.vt.diff
 
 # Exit with success code
 exit 0
