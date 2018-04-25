396367d98120a88f9367ef423392a4f196de97e7
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-04-25 09:09:44 +0000

    Add test for restarting finished training

---
+++ tests/training/restart/test_restarting_finished.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -x
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -rf finish finish_?.log
+mkdir -p finish
+
+
+$MRT_MARIAN/build/marian \
+    -m finish/model.npz -t $MRT_DATA/train.max50.{en,de} -v vocab.en.yml vocab.de.yml \
+    --after-batches 5 --log finish_1.log
+
+test -e finish/model.npz
+test -e finish/model.npz.yml
+test -e finish_1.log
+
+grep -q "Training finished" finish_1.log
+
+$MRT_MARIAN/build/marian -c finish/model.npz.yml --log finish_2.log
+
+grep -q "Loading model" finish_2.log
+grep -q "Training finished" finish_2.log
+
+# Exit with success code
+exit 0
