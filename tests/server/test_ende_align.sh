25b35199ce5f88c5cbf6220f648ff3028c01b72c
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-06-19 12:06:28 +0000

    Add tests for returning alignments by marian-server

---
+++ tests/server/test_ende_align.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+clean_up() {
+    kill $SERVER_PID
+}
+trap clean_up EXIT
+
+# Test code goes here
+$MRT_MARIAN/build/marian-server -c $MRT_MODELS/wmt16_systems/marian.en-de.yml -p 8765 \
+    --alignment > server.align.log 2>&1 &
+SERVER_PID=$!
+
+sleep 20
+
+python3 $MRT_MARIAN/scripts/server/client_example.py -p 8765 < text.in > text.align.out
+kill $SERVER_PID
+
+diff text.align.out text.align.expected > text.align.diff
+
+# Exit with success code
+exit 0
