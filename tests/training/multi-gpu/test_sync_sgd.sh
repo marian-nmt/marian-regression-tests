3b8d8b7db373837f60baf4c7c5c099eb36c50006
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-07-02 14:21:03 +0000

    Add sync-SGD test

---
+++ tests/training/multi-gpu/test_sync_sgd.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+# Exit on error
+set -e
+
+if (( $MRT_NUM_DEVICES < 2 )); then
+    echo "Too few devices available"
+    exit 100
+fi
+
+# Test code goes here
+rm -rf sync_sgd sync_sgd.log
+mkdir -p sync_sgd
+
+$MRT_MARIAN/build/marian \
+    --no-shuffle --seed 777 --mini-batch 4 --maxi-batch 1 --maxi-batch-sort none \
+    --dim-rnn 64 --dim-emb 32 --learn-rate 0.1 \
+    --devices 0 1 --sync-sgd -o sgd \
+    -m sync_sgd/model.full.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
+    --disp-freq 10 --after-batches 100 \
+    --log sync_sgd.log
+
+test -e sync_sgd/model.full.npz
+test -e sync_sgd.log
+
+cat sync_sgd.log | $MRT_TOOLS/extract-costs.sh > sync_sgd.out
+$MRT_TOOLS/diff-floats.py sync_sgd.out sync_sgd.expected -p 0.01 > sync_sgd.diff
+
+# Exit with success code
+exit 0
