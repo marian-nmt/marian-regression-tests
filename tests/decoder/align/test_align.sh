73b96ccbc4af3d1ee314af78ba1b83d985b8f33f
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-07-31 10:19:40 +0100

    Use diff-floats instead of diff

--- tests/decoder/align/test_align.sh
+++ tests/decoder/align/test_align.sh
@@ -5,7 +5,7 @@ set -e
 
 # Test code goes here
 $MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml --mini-batch 16 -b 5 --alignment < text.in > align.out
-diff align.out align.expected > align.diff
+$MRT_TOOLS/diff-floats.py -p 0.0001 align.out align.expected > align.diff
 
 # Exit with success code
 exit 0
