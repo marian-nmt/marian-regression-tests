cf77596eb57822b823cbe261a556e1ec1044ed04
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-08-09 11:37:29 +0100

    Add tests for summarized score

---
+++ tests/scorer/scores/test_summary_perplexity.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Run scorer
+$MRT_MARIAN/build/marian-scorer -c $MRT_MODELS/wmt16_systems/marian.en-de.yml \
+  -m $MRT_MODELS/wmt16_systems/en-de/model.npz \
+  -t $(pwd)/scores.src.in $(pwd)/scores.trg.in --summary perplexity > summary_perplexity.out
+
+# Compare scores
+$MRT_TOOLS/diff-floats.py summary_perplexity.out summary_perplexity.expected -p 0.0003
+
+# Exit with success code
+exit 0
