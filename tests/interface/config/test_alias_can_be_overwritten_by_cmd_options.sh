67877195374c3379789fa21fd5567375bf8e66c5
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-08-29 17:42:07 +0100

    Add tests for aliases

---
+++ tests/interface/config/test_alias_can_be_overwritten_by_cmd_options.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+rm -rf bestdeep_overwrite bestdeep_overwrite.log
+mkdir -p bestdeep_overwrite
+
+
+# Test
+$MRT_MARIAN/build/marian -t $MRT_DATA/europarl.de-en/toy.bpe.{de,en} -m bestdeep_overwrite/model.npz -v vocab.de.yml vocab.en.yml \
+    --type s2s --dim-emb 32 --dim-rnn 16 --mini-batch 1 --after-batches 1 --no-shuffle \
+   --best-deep --enc-depth 6 --dec-depth 6 --log bestdeep_overwrite.log
+
+test -e bestdeep_overwrite.log
+
+grep -q "layer-normalization: true" bestdeep_overwrite.log
+grep -q "tied-embeddings: true" bestdeep_overwrite.log
+grep -q "enc-depth: 6" bestdeep_overwrite.log
+grep -q "dec-depth: 6" bestdeep_overwrite.log
+
+grep -q "type: s2s" bestdeep_overwrite.log
+grep -q "mini-batch: 1" bestdeep_overwrite.log
+
+
+# Exit with success code
+exit 0
