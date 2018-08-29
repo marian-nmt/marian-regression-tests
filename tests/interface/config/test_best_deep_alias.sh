7f518d03a1f06f668c599d0decf8099cbc4a42ba
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-08-29 17:01:09 +0100

    Add test for --best-deep

---
+++ tests/interface/config/test_best_deep_alias.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+rm -rf bestdeep bestdeep.log
+mkdir -p bestdeep
+
+
+# Test
+$MRT_MARIAN/build/marian -t $MRT_DATA/europarl.de-en/toy.bpe.{de,en} -m bestdeep/model.npz -v vocab.de.yml vocab.en.yml \
+    --type s2s --dim-emb 32 --dim-rnn 16 --mini-batch 1 --after-batches 1 --no-shuffle \
+   --best-deep --log bestdeep.log
+
+test -e bestdeep.log
+
+grep -q "best-deep: true" bestdeep.log
+grep -q "layer-normalization: true" bestdeep.log
+grep -q "tied-embeddings: true" bestdeep.log
+grep -q "enc-depth: 4" bestdeep.log
+grep -q "dec-depth: 4" bestdeep.log
+
+grep -q "type: s2s" bestdeep.log
+grep -q "mini-batch: 1" bestdeep.log
+
+
+# Exit with success code
+exit 0
