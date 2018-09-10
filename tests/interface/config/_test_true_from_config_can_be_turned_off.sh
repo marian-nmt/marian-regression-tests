1ba32a7f1708aa831c3e17fcfbcc41706002e279
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-10 17:58:34 +0100

    Add test for bool options

---
+++ tests/interface/config/_test_true_from_config_can_be_turned_off.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+rm -rf booloff booloff.log
+mkdir -p booloff
+
+# Test
+$MRT_MARIAN/build/marian -t $MRT_DATA/europarl.de-en/toy.bpe.{de,en} -m booloff/model.npz -v vocab.de.yml vocab.en.yml \
+    --dim-emb 32 --dim-rnn 16 --mini-batch 1 --after-batches 1 --no-shuffle \
+    --config booloff.yml --layer-normalization --log booloff.log
+
+test -e booloff.log
+
+grep -q "lr-report: true" booloff.log
+grep -q "layer-normalization: false" booloff.log
+
+# Exit with success code
+exit 0
