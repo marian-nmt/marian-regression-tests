87bd5f45cd627e9d83b3860c6ca0aad0d8c7c8ae
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-21 14:49:53 +0100

    Add tests for logging Marian version

--- tests/interface/version/test_model_has_version.sh
+++ tests/interface/version/test_model_has_version.sh
@@ -7,18 +7,23 @@ set -e
 rm -rf version version.log
 mkdir -p version
 
-# Save a model
+# Train a model
 $MRT_MARIAN/build/marian \
     -m version/model.npz \
     -t $MRT_DATA/europarl.de-en/toy.bpe.{en,de} \
     -v version/vocab.en.yml version/vocab.de.yml \
-    --after-batches 1
+    --after-batches 1 --log version.log
+
+# Check if the version is logged for newly started training
+test -e version.log
+grep -qP "created with Marian v[1-9]+\.[0-9]+\.[0-9]+\+.*" version.log
+rm -f version.log
 
 # Check if the model contains a version
 test -e version/model.npz
 python3 $MRT_MARIAN/scripts/contrib/model_info.py -s -m version/model.npz | grep -qP "version: v[1-9]+\.[0-9]+\.[0-9]+\+.*"
 
-# Check if the version is printed
+# Check if the version is printed during decoding
 echo "test" | $MRT_MARIAN/build/marian-decoder \
     -m version/model.npz -v version/vocab.en.yml version/vocab.de.yml \
     --log version.log
---
+++ tests/interface/version/test_no_version_from_old_models.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -rf noversion noversion.log
+mkdir -p noversion
+
+echo "test" | $MRT_MARIAN/build/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml 2> noversion.log
+
+test -e noversion.log
+if grep -q "created with Marian" noversion.log; then
+    exit 1
+fi
+
+# Exit with success code
+exit 0
