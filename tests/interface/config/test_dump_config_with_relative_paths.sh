299567fb9adec45f6264cd53d24278cd7ff7d5f6
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-22 20:51:07 +0100

    Add test for dumping config with relative paths

---
+++ tests/interface/config/test_dump_config_with_relative_paths.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+rm -rf dump_relpaths.yml
+
+# Run with no config file
+$MRT_MARIAN/build/marian-decoder -c relative_paths.yml --mini-batch 8 --dump-config > dump_relpaths.yml
+
+test -e dump_relpaths.yml
+
+grep -q "type: amun" dump_relpaths.yml
+grep -q "mini-batch: 8" dump_relpaths.yml
+grep -q "dim-emb: 500" dump_relpaths.yml
+grep -q "  - .*wmt16_systems/en-de" dump_relpaths.yml
+
+# Exit with success code
+exit 0
