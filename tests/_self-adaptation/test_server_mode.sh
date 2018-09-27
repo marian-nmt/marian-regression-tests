a45cf46b54543703422ac9063ac0eb9360c8c0f1
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-09-27 09:57:17 +0000

    Add test for server-mode self-adaptive translation

---
+++ tests/_self-adaptation/test_server_mode.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+clean_up() {
+    kill $SERVER_PID
+}
+trap clean_up EXIT
+
+# Test code goes here
+$MRT_MARIAN/build/marian-self-adapt -m $MRT_MODELS/wmt16_systems/en-de/model.npz -c $MRT_MODELS/wmt16_systems/marian.en-de.yml -p 8766 > server.log 2>&1 &
+SERVER_PID=$!
+
+sleep 20
+
+python3 $MRT_MARIAN/scripts/server/client_selfadapt.py -p 8766 > text.out
+kill $SERVER_PID
+
+test -e server.log
+grep -q "listening on port 8766" server.log
+grep -q '{"output":"dies ist ein Beispiel' server.log
+grep -q "Ep. 2 : Up. 4 : Sen. 2" server.log
+grep -q "Ep. 2 : Up. 2 : Sen. 1" server.log 
+grep -q "No context" server.log
+grep -q 'dies ist ein Beispiel' text.out
+
+# Exit with success code
+exit 0
