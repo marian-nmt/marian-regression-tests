fec724b1588644b7f8df2e194523b3a167f8c275
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-10-02 13:53:45 +0100

    Add tests for empty contexts in self-adaptive NMT

---
+++ tests/self-adaptive/test_context_empty.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -f nocontext.log
+
+# Run Marian
+$MRT_MARIAN/build/marian-adaptive \
+  -m $MRT_MODELS/wmt16_systems/en-de/model.npz \
+  -v $MRT_MODELS/wmt16_systems/en-de/vocab.en.json -v $MRT_MODELS/wmt16_systems/en-de/vocab.de.json \
+  --dim-vocabs 85000 85000 --dim-emb 500 --after-epochs 1 \
+  -t ubuntu.nocontext.src ubuntu.nocontext.ref --log nocontext.log < ubuntu.src > nocontext.out
+
+# Check outputs
+diff nocontext.out nocontext.expected > nocontext.diff
+
+# Check if the log file does not contain training logs
+grep -q "Ep\." nocontext.log && exit 1
+
+# Exit with success code
+exit 0
---
+++ tests/self-adaptive/test_context_partial.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Test code goes here
+rm -f contextpart.log
+
+# Run Marian
+$MRT_MARIAN/build/marian-adaptive \
+  -m $MRT_MODELS/wmt16_systems/en-de/model.npz \
+  -v $MRT_MODELS/wmt16_systems/en-de/vocab.en.json -v $MRT_MODELS/wmt16_systems/en-de/vocab.de.json \
+  --dim-vocabs 85000 85000 --dim-emb 500 --after-epochs 1 \
+  -t ubuntu.contextpart.src ubuntu.contextpart.ref --log contextpart.log < ubuntu.src > contextpart.out
+
+# Check outputs
+diff contextpart.out contextpart.expected > contextpart.diff
+
+# Check costs
+cat contextpart.log | $MRT_TOOLS/extract-costs.sh > contextpart.costs.out
+$MRT_TOOLS/diff-floats.py -p 0.01 contextpart.costs.out contextpart.costs.expected > contextpart.costs.diff
+
+# Exit with success code
+exit 0
