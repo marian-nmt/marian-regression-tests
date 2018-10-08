543df07de95866fcbd6bcc2e088871a089138d72
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-10-08 12:21:37 +0100

    Add test for expanding relative paths for 'input' if in config file

---
+++ tests/interface/config/test_relative_paths_for_input_in_config_file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+rm -f relpaths_input.log
+mkdir -p subdir
+
+# Test code goes here
+echo "this is a test" > relpaths_input.in
+
+$MRT_MARIAN/build/marian-decoder -c ./subdir/relpaths_input.yml -o relpaths_input.out --log relpaths_input.log
+
+test -e relpaths_input.log
+test -e relpaths_input.out
+
+# Check if relative paths from config files have been expanded
+if ! grep -q "\.\./\.\./" relpaths_input.log; then exit 0; else exit 1; fi
+if ! grep -q "\./subdir/\.\." relpaths_input.log; then exit 0; else exit 1; fi
+
+# Exit with success code
+exit 0
