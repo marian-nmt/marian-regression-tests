6c160e145a61adbc096ad03e463b5812d20ca48b
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-10-26 14:18:37 +0100

    Fix transformer test

--- tests/training/model-types/test_transformer.sh
+++ tests/training/model-types/test_transformer.sh
@@ -10,8 +10,8 @@ mkdir -p transformer
 opts="--no-shuffle --seed 1111 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --optimizer sgd --dim-emb 64 --dim-rnn 128"
 
 $MRT_MARIAN/build/marian \
-    -m transformer/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
-    $opts --disp-freq 1 --after-batches 10 \
+    --type transformer -m transformer/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
+    $opts --disp-freq 10 --after-batches 100 \
     --log transformer.log
 
 test -e transformer/model.npz
