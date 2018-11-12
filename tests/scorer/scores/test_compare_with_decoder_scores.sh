2fbe4937625719b25de99213c7bdd55ccdef6048
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-11-12 17:13:48 +0000

    Add missing -o options

--- tests/scorer/scores/test_compare_with_decoder_scores.sh
+++ tests/scorer/scores/test_compare_with_decoder_scores.sh
@@ -21,7 +21,7 @@ $MRT_MARIAN/build/marian-scorer -c $MRT_MODELS/wmt16_systems/marian.en-de.scorer
 
 # Compare scores
 cat nbest.out | sed 's/ ||| /\t/g' | cut -f4 > compare.decoder.out
-$MRT_TOOLS/diff-nums.py compare.scorer.out compare.decoder.out -p 0.0003
+$MRT_TOOLS/diff-nums.py compare.scorer.out compare.decoder.out -p 0.0003 -o compare.scorer.diff
 
 # Exit with success code
 exit 0
