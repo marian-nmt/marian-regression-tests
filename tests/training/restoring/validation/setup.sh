47a8eac0de6be7e9e7d2128e71bdd43b7cbbfb85
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2018-11-26 15:49:19 +0000

    Update MRT_MARIAN in setup.sh files

--- tests/training/restoring/validation/setup.sh
+++ tests/training/restoring/validation/setup.sh
@@ -7,7 +7,7 @@ test -e $MRT_DATA/train.max50.de
 test -e dev.bpe.de || tail -n 100 $MRT_DATA/europarl.de-en/corpus.bpe.de > dev.bpe.de
 test -e dev.bpe.en || tail -n 100 $MRT_DATA/europarl.de-en/corpus.bpe.en > dev.bpe.en
 
-test -e vocab.de.yml || $MRT_MARIAN/build/marian-vocab < $MRT_DATA/europarl.de-en/corpus.bpe.de > vocab.de.yml
-test -e vocab.en.yml || $MRT_MARIAN/build/marian-vocab < $MRT_DATA/europarl.de-en/corpus.bpe.en > vocab.en.yml
+test -e vocab.de.yml || $MRT_MARIAN/marian-vocab < $MRT_DATA/europarl.de-en/corpus.bpe.de > vocab.de.yml
+test -e vocab.en.yml || $MRT_MARIAN/marian-vocab < $MRT_DATA/europarl.de-en/corpus.bpe.en > vocab.en.yml
 test -e vocab.de.yml
 test -e vocab.en.yml
