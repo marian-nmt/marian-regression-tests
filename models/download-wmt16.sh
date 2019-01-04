fb1b27ca4d3ceb7c398d47ab80c56a2e64fb6be3
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-01-04 11:03:07 +0000

    Download wmt16-deen model

--- models/download-wmt16.sh
+++ models/download-wmt16.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# Download single en-de wmt16 model
-wget -nv -nc -r -e robots=off -nH -np -R *r2l* -R index.html* http://data.statmt.org/wmt16_systems/en-de/
+# Download all en-de wmt16 models
+wget -nv -nc -r -e robots=off -nH -np -R '*r2l*' -R 'index.html*' http://data.statmt.org/wmt16_systems/en-de/
+
+# Download single de-en wmt16 model
+wget -nv -nc -r -e robots=off -nH -np -R '*r2l*' -R '*-ens*' -R '*.sh' -R 'index.html*' http://data.statmt.org/wmt16_systems/de-en/
