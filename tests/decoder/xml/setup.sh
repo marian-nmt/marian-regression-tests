9697a526dfe3ef64eee42981c0fd3e8cf78a1793
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-01-04 16:43:17 +0000

    Add test comparing translation of no-XML input w/o constrained decoding

--- tests/decoder/xml/setup.sh
+++ tests/decoder/xml/setup.sh
@@ -1,1 +1,2 @@
+test -f $MRT_MODELS/wmt16_systems/en-de/model.npz || exit 1
 test -f $MRT_MODELS/wmt16_systems/de-en/model.npz || exit 1
---
+++ tests/decoder/xml/test_compare_xml_input_with_no_tags.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+#####################################################################
+# SUMMARY: Check if constrained decoding does not change an input without XML tags
+# AUTHOR: snukky
+#####################################################################
+
+# Exit on error
+set -e
+
+# Remove old artifacts
+rm -f nbest.{out,diff}
+
+# Run marian decoder
+$MRT_MARIAN/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml --mini-batch 10 -b 4 -n --n-best --xml-input < notags.in > notags.xml.out
+$MRT_MARIAN/marian-decoder -c $MRT_MODELS/wmt16_systems/marian.en-de.yml --mini-batch 10 -b 4 -n --n-best < notags.in > notags.noxml.out
+
+# Compare the output with the expected output
+$MRT_TOOLS/diff-nums.py notags.xml.out notags.noxml.out -o notags.diff
+
+# Exit with success code
+exit 0
