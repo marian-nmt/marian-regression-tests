6f91ca8175a0098936e825ceb2c5a3496a7cc9a9
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-03-07 12:29:53 +0000

    Add -nv to wget command

--- models/download-lm.sh
+++ models/download-lm.sh
@@ -4,7 +4,7 @@ test -e lmgec/lm.npz && exit
 
 mkdir -p lmgec
 cd lmgec
-wget -nc http://data.statmt.org/romang/gec-naacl18/models.tgz
+wget -nv -nc http://data.statmt.org/romang/gec-naacl18/models.tgz
 tar zxvf models.tgz lm.npz tc.model gec.bpe vocab.yml
 rm models.tgz
 cd ..
