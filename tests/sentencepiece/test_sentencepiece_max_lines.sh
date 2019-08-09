db743d7fff2e07d9f839fbb3393dfbb3aabee2cb
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-08-09 15:54:50 +0100

    Add tests for SentencePiece options

---
+++ tests/sentencepiece/test_sentencepiece_max_lines.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+#####################################################################
+# SUMMARY: Create a SentencePiece vocabulary using --sentencepiece-max-lines
+# AUTHOR: snukky
+# TAGS: sentencepiece
+#####################################################################
+
+# Exit on error
+set -e
+
+# Remove old artifacts and create working directory
+rm -rf vocab.maxlines vocab.maxlines.*{log,out,diff}
+mkdir -p vocab.maxlines
+
+# Run marian command
+$MRT_MARIAN/marian \
+    --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --maxi-batch 1 --maxi-batch-sort none --after-batches 1 \
+    -m vocab.maxlines/model.npz -t $MRT_DATA/europarl.de-en/corpus.small.{en,de}.gz \
+    --dim-vocabs 4000 -v vocab.maxlines/vocab.ende.spm vocab.maxlines/vocab.ende.spm --sentencepiece-max-lines 2345 \
+    --log vocab.maxlines.log
+
+# Check if files exist
+test -e vocab.maxlines/model.npz
+test -e vocab.maxlines/vocab.ende.spm
+test -e vocab.maxlines.log
+
+# Check logging messages
+grep -q "Training SentencePiece vocabulary .*vocab.ende.spm" vocab.maxlines.log
+grep -q "Setting vocabulary size .* to 4000" vocab.maxlines.log
+grep -q "Sampling at most 2345 lines from" vocab.maxlines.log
+grep -q "Loading SentencePiece vocabulary .*vocab.ende.spm" vocab.maxlines.log
+
+# Extract a textual vocabulary and compare with the expected output
+$MRT_MARIAN/spm_export_vocab --model vocab.maxlines/vocab.ende.spm > vocab.maxlines.out
+$MRT_TOOLS/diff-nums.py vocab.maxlines.out vocab.maxlines.expected -o vocab.maxlines.diff
+
+# Exit with success code
+exit 0
---
+++ tests/sentencepiece/test_sentencepiece_normalization.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+#####################################################################
+# SUMMARY: Create a SentencePiece vocabulary with normalization rules
+# AUTHOR: snukky
+# TAGS: sentencepiece
+#####################################################################
+
+# Exit on error
+set -e
+
+# Remove old artifacts and create working directory
+rm -rf vocab.norm vocab.norm.*{log,out,diff}
+mkdir -p vocab.norm
+
+# Run marian command
+$MRT_MARIAN/marian \
+    --no-shuffle --seed 2222 --dim-emb 32 --dim-rnn 64 --maxi-batch 1 --maxi-batch-sort none --after-batches 1 \
+    -m vocab.norm/model.npz -t $MRT_DATA/europarl.de-en/corpus.small.{en,de}.gz \
+    --dim-vocabs 4000 -v vocab.norm/vocab.ende.spm vocab.norm/vocab.ende.spm --sentencepiece-options '--normalization_rule_tsv=norm.tsv' --sentencepiece-max-lines 10000 \
+    --log vocab.norm.log
+
+# Check if files exist
+test -e vocab.norm/model.npz
+test -e vocab.norm/vocab.ende.spm
+test -e vocab.norm.log
+
+# Check logging messages
+grep -q "Training SentencePiece vocabulary .*vocab.ende.spm" vocab.norm.log
+
+# Extract a textual vocabulary and compare with the expected output
+$MRT_MARIAN/spm_export_vocab --model vocab.norm/vocab.ende.spm > vocab.norm.out
+$MRT_TOOLS/diff-nums.py vocab.norm.out vocab.norm.expected -o vocab.norm.diff
+
+# Normalization is uppercasing, so check if there is no lowercased ASCII characters
+grep -qvP '[a-z]' vocab.norm.out 
+
+# Exit with success code
+exit 0
