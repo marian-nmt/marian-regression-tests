6a81b5d7edb97c71ce9eba81ba544d115d6fd01f
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-08-11 18:09:01 +0100

    Uncomment tests for aliases

--- tests/interface/config/test_load_config_with_alias.sh
+++ tests/interface/config/test_load_config_with_alias.sh
@@ -13,7 +13,8 @@ rm -rf load_alias load_alias.log
 mkdir -p load_alias
 
 # Run Marian
-$MRT_MARIAN/marian --train-sets $MRT_DATA/europarl.de-en/corpus.bpe.{de,en} --model load_alias/model.npz --vocabs vocab.de.yml vocab.en.yml --no-shuffle \
+$MRT_MARIAN/marian --no-shuffle --train-sets $MRT_DATA/europarl.de-en/corpus.bpe.{de,en} --model load_alias/model.npz \
+    --vocabs vocab.deen.yml vocab.deen.yml --dim-vocabs 4000 4000 \
     --config load_alias.yml --log load_alias.log
 
 test -e load_alias/model.npz
