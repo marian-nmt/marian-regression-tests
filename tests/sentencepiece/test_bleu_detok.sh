142c12f0e7f0dac5ec8104f145ad0047f091457c
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-10-16 15:54:05 +0000

    Use sacreBLEU from the repository

--- tests/sentencepiece/test_bleu_detok.sh
+++ tests/sentencepiece/test_bleu_detok.sh
@@ -38,7 +38,7 @@ $MRT_TOOLS/diff.sh bleu-detok.bleu bleu-detok.bleu.expected > bleu-detok.bleu.di
 
 
 # Run sacreBLEU removing the version information
-sacrebleu dev.en < bleu-detok.out | sed -r 's/.version[^ ]* / /' > bleu-detok.sacrebleu
+$MRT_TOOLS/sacrebleu/sacrebleu.py dev.en < bleu-detok.out | sed -r 's/.version[^ ]* / /' > bleu-detok.sacrebleu
 # Check BLEU from the validation translation output 
 $MRT_TOOLS/diff.sh bleu-detok.sacrebleu bleu-detok.sacrebleu.expected > bleu-detok.sacrebleu.diff
 
