b385a9180b9da0390546f9cf2ee6a6ea16ca7346
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2019-12-17 13:48:00 +0000

    Make a single test for AVX2 and AVX-512

--- tests/models/wngt19/test_model_base_fbgemm_packed8.sh
+++ tests/models/wngt19/test_model_base_fbgemm_packed8.sh
@@ -7,30 +7,37 @@ set -e
 if [ ! $MRT_MARIAN_USE_FBGEMM ]; then
     echo "Marian is not compiled with FBGEMM" 1>&2
     exit 100
-elif ! grep -q "avx512" /proc/cpuinfo; then
-    echo "Your CPU does not support AVX-512, which is required" 1>&2
+elif ! grep -q "avx2" /proc/cpuinfo; then
+    echo "Your CPU does not support AVX2, which is required" 1>&2
     exit 100
 fi
 
-prefix=model_base_fbgemm_packed8_avx512
+
+# Outputs differ on CPUs supporting AVX2 or AVX512
+suffix=avx2
+if grep -q "avx512" /proc/cpuinfo; then
+    suffix=avx512
+fi
+
+prefix=model_base_fbgemm_packed8
 
 # Remove previous outputs
-rm -f $prefix.out $prefix.bin
+rm -f $prefix.out $prefix.$suffix.bin
 
 # Pack the model
-$MRT_MARIAN/marian-conv -f $MRT_MODELS/wngt19/model.base.npz -t $prefix.bin --gemm-type packed8
-test -s $prefix.bin
+$MRT_MARIAN/marian-conv -f $MRT_MODELS/wngt19/model.base.npz -t $prefix.$suffix.bin --gemm-type packed8
+test -s $prefix.$suffix.bin
 
 # Run test
 $MRT_MARIAN/marian-decoder \
-    -m $prefix.bin -v $MRT_MODELS/wngt19/en-de.spm $MRT_MODELS/wngt19/en-de.spm \
+    -m $prefix.$suffix.bin -v $MRT_MODELS/wngt19/en-de.spm $MRT_MODELS/wngt19/en-de.spm \
     -i newstest2014.in -o $prefix.out \
     -n 0.6 -b 1 --shortlist $MRT_MODELS/wngt19/lex.s2t.gz --skip-cost --cpu-threads 1 \
     --mini-batch 24 --maxi-batch 100 --maxi-batch-sort src -w 512 \
     --max-length 150 --max-length-crop --quiet-translation
 
 # Compare with the expected output
-$MRT_TOOLS/diff.sh $prefix.out $prefix.expected > $prefix.diff
+$MRT_TOOLS/diff.sh $prefix.out $prefix.$suffix.expected > $prefix.diff
 
 
 # Exit with success code
