1730727b0a6da25427217bc194f534d844290a84
Roman Grundkiewicz <rgrundki@exseed.ed.ac.uk>
2020-01-24 10:05:57 +0000

    Add test for scoring with blank lines

---
+++ tests/interface/input/test_score_with_blank_lines.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+#####################################################################
+# SUMMARY: Re-score files with empty lines
+# AUTHOR: snukky
+# TAGS: emptyline sentencepiece scorer
+#####################################################################
+
+# Exit on error
+set -e
+
+# Remove old artifacts
+rm -f blank_score.{out,diff}
+
+# Run marian scorer
+$MRT_MARIAN/marian-scorer -c $MRT_MODELS/rnn-spm/score.yml -t text_blank_lines.{in,ref} > blank_score.out
+
+# Compare the output with the expected output
+$MRT_TOOLS/diff.sh blank_score.out blank_score.expected > blank_score.diff
+
+# Exit with success code
+exit 0
