4abbfe39580f520ae0d7659c996d251c025876c4
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2020-10-21 03:40:02 -0700

    Update tests for other training features

--- tests/training/features/guided-alignment/test_guided_alignment_rnn.sh
+++ tests/training/features/guided-alignment/test_guided_alignment_rnn.sh
@@ -15,7 +15,7 @@ mkdir -p rnn
 
 # Run marian command
 $MRT_MARIAN/marian \
-    --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --optimizer sgd \
+    --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --optimizer sgd --cost-type ce-mean \
     -m rnn/model.npz -t corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
     --after-batches 100 --disp-freq 10 \
     --guided-alignment corpus.bpe.align --guided-alignment-weight 1.0 --learn-rate 0.1 \
--- tests/training/features/right-left/test_right_left_rnn.sh
+++ tests/training/features/right-left/test_right_left_rnn.sh
@@ -14,7 +14,7 @@ mkdir -p rnn
 
 # Run marian command
 $MRT_MARIAN/marian \
-    --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --optimizer sgd \
+    --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --optimizer sgd --cost-type ce-mean \
     -m rnn/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
     --after-batches 100 --disp-freq 10 \
     --right-left --log rnn.log
