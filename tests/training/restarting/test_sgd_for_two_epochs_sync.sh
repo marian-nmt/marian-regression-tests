0ca3c2877f205170db6260ef379162bccfea35f1
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2020-10-21 04:58:25 -0700

    Update tests for multi-gpu training

--- tests/training/restarting/test_sgd_for_two_epochs_sync.sh
+++ tests/training/restarting/test_sgd_for_two_epochs_sync.sh
@@ -8,6 +8,8 @@ rm -rf sgd_sync_2e sgd_sync_*_epoch.log
 mkdir -p sgd_sync_2e
 
 extra_opts="--no-shuffle --seed 1111 --maxi-batch 1 --maxi-batch-sort none --mini-batch 32 --optimizer sgd --sync-sgd"
+# Added because default options has changes
+extra_opts="$extra_opts --cost-type ce-mean --disp-label-counts false"
 
 
 # Uncomment to prepare the expected output
