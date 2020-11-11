4dac388a39adc581ecba6ae88b5a889988d15729
Alham Fikri Aji <afaji321@gmail.com>
2020-11-11 04:27:38 +0000

    simplify quantized-model testing by using a shared train folder

--- tests/training/features/quantized-model/test_quantmodel.sh
+++ tests/training/features/quantized-model/test_quantmodel.sh
@@ -11,18 +11,18 @@ set -e
 PREFIX=quantized
 
 # Remove old artifacts and create working directory
-rm -rf $PREFIX $PREFIX.{log,out,diff}
-mkdir -p $PREFIX train
+rm -rf train $PREFIX.{log,out,diff}
+mkdir -p train
 
 # Train an 8-bits model
 $MRT_MARIAN/marian \
     --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --learn-rate 0.1 --optimizer sgd \
-    -m $PREFIX/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
+    -m train/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
     --cost-type cross-entropy --sync-sgd --after-batches 100 --disp-freq 10 --quantize-bits 8 \
     --log $PREFIX.log
 
 # Check if files exist
-test -e $PREFIX/model.npz
+test -e train/model.npz
 test -e $PREFIX.log
 
 # Compare the current output with the expected output
@@ -30,7 +30,7 @@ cat $PREFIX.log | $MRT_TOOLS/extract-costs.sh > $PREFIX.out
 $MRT_TOOLS/diff-nums.py $PREFIX.out $PREFIX.expected -o $PREFIX.diff
 
 # make sure that the resulting model has no more than 256 different values (i.e. quantized)
-$MRT_TOOLS/check-model-unique-vals.py $PREFIX/model.npz -b 8
+$MRT_TOOLS/check-model-unique-vals.py train/model.npz -b 8
 
 # Exit with success code
 exit 0
--- tests/training/features/quantized-model/test_quantmodel_with_bias.sh
+++ tests/training/features/quantized-model/test_quantmodel_with_bias.sh
@@ -11,25 +11,25 @@ set -e
 PREFIX=quantized-with-bias
 
 # Remove old artifacts and create working directory
-rm -rf $PREFIX $PREFIX.{log,out,diff}
-mkdir -p $PREFIX train
+rm -rf train $PREFIX.{log,out,diff}
+mkdir -p train
 
 # training with quantized bias is tricky, so we start by training a normal model first before finetuning it to the quantized space.
 $MRT_MARIAN/marian \
     --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --learn-rate 0.1 --optimizer sgd \
-    -m $PREFIX/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
+    -m train/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
     --cost-type cross-entropy --sync-sgd --after-batches 20 --disp-freq 10 \
     --log $PREFIX.log
 
 # Train an 8-bits model
 $MRT_MARIAN/marian \
     --no-shuffle --seed 1111 --dim-emb 32 --dim-rnn 64 --mini-batch 32 --maxi-batch 1 --maxi-batch-sort none --learn-rate 0.1 --optimizer sgd \
-    -m $PREFIX/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
+    -m train/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v train/vocab.en.yml train/vocab.de.yml \
     --cost-type cross-entropy --sync-sgd --after-batches 100 --disp-freq 10 --quantize-bits 8 --quantize-biases \
     --log $PREFIX.log
 
 # Check if files exist
-test -e $PREFIX/model.npz
+test -e train/model.npz
 test -e $PREFIX.log
 
 # Compare the current output with the expected output
@@ -37,7 +37,7 @@ cat $PREFIX.log | $MRT_TOOLS/extract-costs.sh > $PREFIX.out
 $MRT_TOOLS/diff-nums.py $PREFIX.out $PREFIX.expected -o $PREFIX.diff
 
 # make sure that the resulting model has no more than 256 different values (i.e. quantized)
-$MRT_TOOLS/check-model-unique-vals.py $PREFIX/model.npz -b 8 --with_bias
+$MRT_TOOLS/check-model-unique-vals.py train/model.npz -b 8 --with_bias
 
 # Exit with success code
 exit 0
