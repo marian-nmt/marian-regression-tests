8d5756fe53745978f8c5c9c51c25d52357d03095
Roman Grundkiewicz <rgrundkiewicz@gmail.com>
2020-11-13 09:40:28 -0800

    Use clip-norm 0 in training/multi-gpu

--- tests/training/multi-gpu/test_sync_sgd_1gpu_expsmooth.sh
+++ tests/training/multi-gpu/test_sync_sgd_1gpu_expsmooth.sh
@@ -8,8 +8,8 @@ rm -rf sync_sgd_1gpu_expsmooth sync_sgd_1gpu_expsmooth.log
 mkdir -p sync_sgd_1gpu_expsmooth
 
 $MRT_MARIAN/marian \
-    --no-shuffle --seed 888 --mini-batch 4 --maxi-batch 1 --maxi-batch-sort none \
-    --dim-rnn 64 --dim-emb 32 --learn-rate 0.1 \
+    --no-shuffle --clip-norm 0 --seed 888 --mini-batch 4 --maxi-batch 1 --maxi-batch-sort none \
+    --dim-rnn 64 --dim-emb 32 --learn-rate 0.02 \
     --devices 0 --sync-sgd --optimizer sgd --exponential-smoothing --cost-type ce-mean \
     -m sync_sgd_1gpu_expsmooth/model.npz -t $MRT_DATA/europarl.de-en/corpus.bpe.{en,de} -v vocab.en.yml vocab.de.yml \
     --disp-freq 5 --save-freq 10 --after-batches 20 \
@@ -19,7 +19,7 @@ test -e sync_sgd_1gpu_expsmooth/model.npz
 test -e sync_sgd_1gpu_expsmooth.log
 
 cat sync_sgd_1gpu_expsmooth.log | $MRT_TOOLS/extract-costs.sh > sync_sgd_1gpu_expsmooth.out
-$MRT_TOOLS/diff-nums.py sync_sgd_1gpu_expsmooth.out sync_sgd_1gpu_expsmooth.expected -o sync_sgd_1gpu_expsmooth.diff
+$MRT_TOOLS/diff-nums.py -p 0.02 sync_sgd_1gpu_expsmooth.out sync_sgd_1gpu_expsmooth.expected -o sync_sgd_1gpu_expsmooth.diff
 
 # Exit with success code
 exit 0
