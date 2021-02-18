4b11c2e2c9fb03acdefd3dbd1f81b66c34f02d09
Pedro Coelho <pedrodiascoelho97@gmail.com>
2021-02-18 15:35:19 +0000

    remove debugging line

--- tests/decoder/factors/setup.sh
+++ tests/decoder/factors/setup.sh
@@ -7,8 +7,6 @@
 # Exit on error
 set -e
 
-MRT_MODELS=~/marian-dev/regression-tests/models
-
 # Test code goes here
 test -f $MRT_MODELS/factors/model.npz || exit 1
 test -f $MRT_MODELS/factors/model.npz.decoder.yml || exit 1
