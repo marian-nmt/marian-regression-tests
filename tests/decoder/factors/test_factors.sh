3fca60b415155ee743002adae1f43c5a938de936
Pedro Coelho <pedrodiascoelho97@gmail.com>
2021-02-23 13:28:08 +0000

    add verification of factors activation on the decoder test

--- tests/decoder/factors/test_factors.sh
+++ tests/decoder/factors/test_factors.sh
@@ -10,10 +10,14 @@
 set -e
 
 # Remove old artifacts
-rm -f factors.{out,diff}
+rm -f factors.{out,diff,log}
 
 # Run marian decoder
-$MRT_MARIAN/marian-decoder -c $MRT_MODELS/factors/model.npz.decoder.yml < text.in > factors.out
+$MRT_MARIAN/marian-decoder -c $MRT_MODELS/factors/model.npz.decoder.yml --log factors.log < text.in > factors.out
+
+#checks factors usage
+grep -q "Factored embeddings enabled" factors.log
+grep -q "Factored outputs enabled" factors.log
 
 # Compare the output with the expected output
 $MRT_TOOLS/diff.sh factors.out factors.expected > factors.diff
